deca51d1eff876d28609b413631a2de09b30cdf9
fremydev <fremy.dev@gmail.com>
2023-07-14 01:18:22 -0300

    refactor

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -7,9 +7,7 @@ const JUMPSPEED : int = 360
 const JUMPTERMULT: int = 4
 
 func _process(delta):
-	var move_vector: Vector2 = Vector2.ZERO
-	move_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	move_vector.y = -1 if Input.is_action_just_pressed("jump") else 0 
+	var move_vector: Vector2 = get_movement_vector()
 	
 	velocity.x += move_vector.x * HACCELERATION * delta 
 	if move_vector.x == 0:
@@ -24,3 +22,9 @@ func _process(delta):
 	else:
 		velocity.y += GRAVITY * delta
 	move_and_slide()
+
+func get_movement_vector() -> Vector2:
+	var move_vector: Vector2 = Vector2.ZERO
+	move_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	move_vector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return move_vector
